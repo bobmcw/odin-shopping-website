3cd2637d992afbebac646d02ca6d6294e43b4cdc
bobmcw <jasiekz2014@gmail.com>
2025-02-28 16:16:58 +0100

    organized components and styles and removed placeholder

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import './styles/index.css'
+import App from './components/App.jsx'
+import Navbar from './components/Navbar.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Navbar />
     <App />
   </StrictMode>,
 )
