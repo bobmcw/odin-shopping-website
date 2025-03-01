3630cf48747b72ad935bab2793e2ba4bdfd97db1
bobmcw <jasiekz2014@gmail.com>
2025-03-01 16:50:41 +0100

    created a basic router

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './styles/index.css'
 import App from './components/App.jsx'
 import Navbar from './components/Navbar.jsx'
+import routes from './routes.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter(routes);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Navbar />
-    <App />
+   <RouterProvider router={router}/> 
   </StrictMode>,
 )
---
+++ src/routes.jsx
@@ -0,0 +1,9 @@
+import App from "./components/App";
+
+const routes = [
+    {
+        path: "/",
+        element: <App />,
+    },
+];
+export default routes
