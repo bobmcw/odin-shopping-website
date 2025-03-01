3580e06c5c73ef98b27058a5c9e66b8ec50286bd
bobmcw <jasiekz2014@gmail.com>
2025-03-01 16:50:57 +0100

    removed unneeded import

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/index.css'
-import App from './components/App.jsx'
 import Navbar from './components/Navbar.jsx'
 import routes from './routes.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
