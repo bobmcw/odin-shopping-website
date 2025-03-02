57c7d087579f36c4ceeb4e4241888e710272bed3
bobmcw <jasiekz2014@gmail.com>
2025-03-02 15:07:29 +0100

    removed header

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -13,7 +13,6 @@ function App() {
   }, []);
   return (
     <div className={styles.page}>
-      <h1>Store</h1>
       <Banner backgroundImg={banner} />
       <ImageWithText imageUrl={placeholder1} alt="new arrivals" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 
