5ee4fcff446d081d42db38c9a7300ab19db365fc
bobmcw <jasiekz2014@gmail.com>
2025-03-02 20:16:18 +0100

    changed products to display one per line

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -3,8 +3,10 @@ function ProductCard({imageUrl, name, description, action}) {
   return (
     <div className={styles.card}>
         <img src={imageUrl} alt="" />
-        <h3>{name}</h3>
-        <p>{description}</p>
+        <div className={styles.cardInfo}>
+            <h3>{name}</h3>
+            <p>{description}</p>
+        </div>
         {action}
     </div>
   );
