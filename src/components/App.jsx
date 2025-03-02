7aecdd3360682d06c8d95fa5c537f740861d801e
bobmcw <jasiekz2014@gmail.com>
2025-03-02 15:07:11 +0100

    added placeholder data for text with image and adjusted styling

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,7 @@ import styles from "../styles/App.module.css";
 import { useEffect } from "react";
 import ImageWithText from "./ImageWithText";
 import banner from "../assets/banner.jpeg"
+import placeholder1 from "../assets/placeholder1.jpeg"
 
 function App() {
   useEffect(() => {
@@ -14,7 +15,9 @@ function App() {
     <div className={styles.page}>
       <h1>Store</h1>
       <Banner backgroundImg={banner} />
-      <ImageWithText />
+      <ImageWithText imageUrl={placeholder1} alt="new arrivals" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+
+/>
     </div>
   );
 }
