99e547ccb3fcd4110049b546a963d088d82d6a1d
bobmcw <jasiekz2014@gmail.com>
2025-03-02 11:01:29 +0100

    added fetching data from fake shop API

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,19 @@
-import Banner from './Banner'
-import styles from '../styles/App.module.css'
+import Banner from "./Banner";
+import styles from "../styles/App.module.css";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products/categories")
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }, []);
   return (
     <div className={styles.page}>
       <h1>Store</h1>
       <Banner />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
