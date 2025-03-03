8a20b273e3ebdfa31abda57b5ef0981cfd6b3c13
bobmcw <jasiekz2014@gmail.com>
2025-03-03 16:25:12 +0100

    added category checkboxes to the sidenav of products page

--- src/components/ProductsPage.jsx
+++ src/components/ProductsPage.jsx
@@ -1,25 +1,38 @@
 import { useEffect, useState } from "react";
 import ProductCard from "./ProductCard";
-import styles from "../assets/ProductsPage.module.css"
+import styles from "../assets/ProductsPage.module.css";
 import useProductLoader from "../hooks/useProducts";
 function ProductsPage() {
-
-  const {error, loading, products, categories } = useProductLoader()
-  console.log(categories);
+  const { error, loading, products, categories } = useProductLoader();
   return (
     <>
-    <nav className={styles.sideNav}>sideNav</nav>
-    <div className={styles.productsPage}>
-      {products.map((product) => {
-        return (
-          <ProductCard
-            imageUrl={product.image}
-            name={product.title}
-            description={product.description}
-          />
-        );
-      })}
-    </div>
+      <nav className={styles.sideNav}>
+        {categories.map((category) => {
+          return (
+            <div className="cateogory">
+              <input
+                type="checkbox"
+                id={category}
+                name={category}
+                checked
+              ></input>
+              <label htmlFor={category}>{category}</label>
+            </div>
+          );
+        })}
+      </nav>
+      <div className={styles.productsPage}>
+        {products.map((product) => {
+          return (
+            <ProductCard
+              imageUrl={product.image}
+              name={product.title}
+              description={product.description}
+              key={product.id}
+            />
+          );
+        })}
+      </div>
     </>
   );
 }
