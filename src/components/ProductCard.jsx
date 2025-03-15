ef629f61cecf6f259231a0ea851579b10a5097c5
bobmcw <jasiekz2014@gmail.com>
2025-03-15 12:43:51 +0100

    added tabindex to product cards

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,7 +1,7 @@
 import styles from "../styles/ProductCard.module.css";
 function ProductCard({imageUrl, name, description, action, visible=true}) {
   return (
-    <div className={styles.card} style={{display: visible ? "flex" : "none"}}>
+    <div className={styles.card} style={{display: visible ? "flex" : "none"}} tabIndex={0}>
         <img src={imageUrl} alt="" />
         <div className={styles.cardInfo}>
             <h3>{name}</h3>
