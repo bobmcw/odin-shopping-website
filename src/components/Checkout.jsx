512958b3507c2b9414df6a9810b16c0a547f36d6
bobmcw <jasiekz2014@gmail.com>
2025-03-22 13:21:58 +0100

    added a delete button

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -5,6 +5,7 @@ import { useOutletContext } from "react-router-dom";
 import AppleIcon from '@mui/icons-material/Apple';
 import GoogleIcon from '@mui/icons-material/Google';
 import CreditCardIcon from '@mui/icons-material/CreditCard';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 function Checkout() {
   const [cart, setCart] = useOutletContext();
@@ -22,16 +23,27 @@ function Checkout() {
       <div className={styles.checkout}>
         <div className={styles.productsList}>
           {Array.from(cart).map((item) => {
-            return <>
+            return (
+            <div className={styles.cards} key={item[0]} >
                <ProductCard name={item[0]} price={item[1].price} imageUrl={item[1].imageUrl} interactive={false}/> 
-            </>;
+               <div className={styles.controls}>
+                   <button onClick={() => {
+                    const newCart = cart;
+                    newCart.delete(item[0]);
+                    setCart(newCart);
+                    }}>
+                       <DeleteIcon className={styles.delete} />
+                   </button>
+               </div>
+            </div>
+            );
           })}
         </div>
         <div className={styles.summary}>
             <div className={styles.itemList}>
                 {Array.from(cart).map((item) => {
                     return(
-                        <div className={styles.summaryItem}>
+                        <div className={styles.summaryItem} key={item[0] + 'amount'}>
                             <p className={styles.summaryName}>{item[0]} x {item[1].amount} </p>
                             <p className={styles.price}>${item[1].amount * item[1].price}</p>
                         </div>
