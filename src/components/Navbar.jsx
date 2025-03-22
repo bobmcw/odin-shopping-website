5040a6dba520d382c64f9d7a5e9abb3f6106ebbc
bobmcw <jasiekz2014@gmail.com>
2025-03-22 20:17:19 +0100

    made the checkout black and positioned it to the right

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import styles from '../styles/Navbar.module.css';
 import { Link } from 'react-router-dom';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 
 function Navbar() {
     return(
@@ -7,8 +8,8 @@ function Navbar() {
             <div className={styles.links}>
                 <Link to="/">Main page</Link>
                 <Link to="/products">Products</Link>
-                <Link to="/checkout">checkout</Link>
             </div>
+            <div className={styles.icon}><Link to="/checkout"><ShoppingCartIcon sx={{color: 'black'}} /></Link></div>
         </nav>
     );
 }
